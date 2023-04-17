4ffe272f7a90d8124973335cbe7fe6ac0f090a09
Alcedo <zyc404@gmail.com>
2023-04-17 10:16:45 +0800

    样式修正

--- src/components/Cards/CardPageVisits.vue
+++ src/components/Cards/CardPageVisits.vue
@@ -50,7 +50,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in userData" :key="item.herf">
+          <tr v-for="item in pageData" :key="item.herf">
             <th
               class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left"
             >
@@ -69,11 +69,32 @@
             <td
               class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
             >
-              {{item.time}}
+              {{new Date(item.time).toLocaleString('zh', { hour12: false }).replaceAll('/', '-')}}
             </td>
           </tr>
         </tbody>
       </table>
+    </div>
+    <div class="py-2 px-4">
+      <nav class="block">
+        <ul class="flex pl-0 rounded list-none flex-wrap">
+          <li class="px-2">
+            <a @click="page > 1 ? page -- : page" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-sky-500 bg-white text-sky-500">
+              <i class="fas fa-chevron-left -ml-px"></i>
+            </a>
+          </li>
+          <li v-for="i in Math.ceil(userData.length / 10)" class="px-2">
+            <a @click="page = i" :class="page === i ? 'bg-emerald-200' : 'bg-blueGray-50'" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-sky-500 text-sky-500">
+              {{ i }}
+            </a>
+          </li>
+          <li class="px-2">
+            <a @click="page < Math.ceil(userData.length / 10) ? page ++ : page" class="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-sky-500 bg-white text-sky-500">
+              <i class="fas fa-chevron-right -mr-px"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -83,11 +104,258 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      userData: []
+      userData: [
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/j92qz",
+    "model": "Bing",
+    "time": "2023-04-13T10:28:36.258Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/vlsi0",
+    "model": "Bing",
+    "time": "2023-04-13T10:56:22.938Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/hpndy",
+    "model": "Bing",
+    "time": "2023-04-13T13:35:51.451Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/2m0x1",
+    "model": "Bing",
+    "time": "2023-04-14T07:34:07.210Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/hwds1",
+    "model": "Bing",
+    "time": "2023-04-14T09:27:25.157Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/8z4pg",
+    "model": "Bing",
+    "time": "2023-04-14T16:27:45.801Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/b1pcy",
+    "model": "Bing",
+    "time": "2023-04-14T16:28:37.048Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/jc7ph",
+    "model": "Bing",
+    "time": "2023-04-14T16:35:26.388Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "踩蘑菇mod学习小组",
+    "herf": "http://cache.alcedogroup.com/page/awkgs",
+    "model": "Bing",
+    "time": "2023-04-15T06:34:52.368Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "踩蘑菇mod学习小组",
+    "herf": "http://cache.alcedogroup.com/page/wiqkf",
+    "model": "Bing",
+    "time": "2023-04-15T06:35:58.020Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/xgje0",
+    "model": "Bing",
+    "time": "2023-04-15T07:26:53.401Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "踩蘑菇mod学习小组",
+    "herf": "http://cache.alcedogroup.com/page/fsytv",
+    "model": "Bing",
+    "time": "2023-04-15T15:06:28.072Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/mdec6",
+    "model": "Bing",
+    "time": "2023-04-15T16:10:47.518Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "踩蘑菇mod学习小组",
+    "herf": "http://cache.alcedogroup.com/page/wd5yx",
+    "model": "Bing",
+    "time": "2023-04-16T09:13:26.197Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "ChatGPT",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/zp14i",
+    "model": "ChatGPT",
+    "time": "2023-04-16T09:21:54.376Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "踩蘑菇mod学习小组",
+    "herf": "http://cache.alcedogroup.com/page/vnc6l",
+    "model": "Bing",
+    "time": "2023-04-16T09:40:30.939Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/bxu5d",
+    "model": "Bing",
+    "time": "2023-04-16T14:13:05.792Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/8g2tx",
+    "model": "Bing",
+    "time": "2023-04-16T14:23:53.273Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/xy4ms",
+    "model": "Bing",
+    "time": "2023-04-16T14:24:56.770Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/5jrzk",
+    "model": "Bing",
+    "time": "2023-04-16T14:27:32.684Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/ll10l",
+    "model": "Bing",
+    "time": "2023-04-17T00:53:06.670Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/2h0tn",
+    "model": "Bing",
+    "time": "2023-04-17T01:07:29.804Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/10xxz",
+    "model": "Bing",
+    "time": "2023-04-17T01:09:51.888Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/6yzhs",
+    "model": "Bing",
+    "time": "2023-04-17T01:10:45.013Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "",
+    "herf": "http://cache.alcedogroup.com/page/canwa",
+    "model": "Bing",
+    "time": "2023-04-17T01:11:17.078Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "ChatGPT",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/zqeks",
+    "model": "ChatGPT",
+    "time": "2023-04-17T01:36:57.190Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "ChatGPT",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/fb3ug",
+    "model": "ChatGPT",
+    "time": "2023-04-17T01:37:23.437Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "Bing",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/x26br",
+    "model": "Bing",
+    "time": "2023-04-17T01:38:35.267Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "ChatGPT",
+    "group": "柴特寄批踢&meme",
+    "herf": "http://cache.alcedogroup.com/page/ch7nu",
+    "model": "ChatGPT",
+    "time": "2023-04-17T01:40:25.135Z"
+  },
+  {
+    "user": "Alcedo",
+    "bot": "ChatGPT",
+    "group": "踩蘑菇mod学习小组",
+    "herf": "http://cache.alcedogroup.com/page/am1t6",
+    "model": "ChatGPT",
+    "time": "2023-04-17T01:44:03.792Z"
+  }
+      ],
+      page: 1,
     }
   },
   created() {
     this.getData()
+  },
+  computed: {
+    pageData() {
+      return this.userData.slice((this.page - 1) * 10, (this.page - 1) * 10 + 10)
+    }
   },
   methods: {
     getData: function() {
@@ -103,7 +371,7 @@ export default {
       .then(() => {
         this.getData()
       })
-    }
+    },
   },
 }
 </script>
